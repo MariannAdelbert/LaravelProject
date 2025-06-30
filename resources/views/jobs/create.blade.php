813ed5781ae9218e05e03c46edd78a409bd87be1
Mariann Adelbert <mariannadelbert@gmail.com>
2025-06-30 11:44:19 +0300

    Day 22: Make a Login and Registration System From Scratch: Part 1

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,17 +12,11 @@
       <p class="mt-1 text-sm/6 text-gray-600">We just need a handful of details from you.</p>
 
       <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="sm:col-span-4">
-          <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+          <x-form-label for="title">Title</x-form-label>
           <div class="mt-2">
-            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Shift Leader" required>
-            </div>
+            <x-form-input name="title" id="title" placeholder="CEO" required />
 
-            @error('title')
-                <p class="text-xs text-red-500 font-semibold">{{ $message }} </p>
-            @enderror
-          </div>
+            <x-form-error name="title" />
         </div>
 
         <div class="sm:col-span-4">
@@ -50,7 +44,7 @@
 
   <div class="mt-6 flex items-center justify-end gap-x-6">
     <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+    <x-form-button>Save</x-form-button>
   </div>
 </form>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Expr\Cast\Array_;
@@ -11,3 +13,9 @@ Route::view('/contact', 'contact');
 Route::resource('jobs', JobController::class);
 
 //Day 20 tegime teise ajutise app projecti
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
