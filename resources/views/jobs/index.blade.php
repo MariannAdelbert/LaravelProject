f4b2080a2234dbef8f8fb597dca9df4746981826
Mariann Adelbert <mariannadelbert@gmail.com>
2025-07-01 11:02:38 +0300

    Day 26: Get Your Build Process in Order

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot:heading>
-        Job page
+        Job Listings
     </x-slot:heading>
 
 <div class="space-y-4">
@@ -8,7 +8,7 @@
         <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
         <div class="font-bold text-blue-500">{{ $job->employer->name }}</div>
         <div>
-            <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary']}} per year.
+             <strong class="text-laracasts">{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
         </div>
         </a>
     @endforeach
