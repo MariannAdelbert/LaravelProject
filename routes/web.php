58e840a8922744e4fa8ca95f9d3efc6fcde031bf
Mariann Adelbert <mariannadelbert@gmail.com>
2025-07-01 15:25:14 +0300

    Day 27: https://github.com/MariannAdelbert/pixel-positions.git

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store'])->name('login');
 Route::post('/logout', [SessionController::class, 'destroy']);
+
+//Day27: https://github.com/MariannAdelbert/pixel-positions.git
